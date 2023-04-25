e53ed66a29d4c129e92ba33c5ed3a4f5ac1a0a9b
Shivani <shivanipatil270@gmail.com>
2023-04-25 22:21:48 +0530

    Searched products save in a variable

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -3,17 +3,20 @@ import axios from 'axios';
 
 const SearchProducts = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState('');
 
   const handleInputChange = (event) => {
+    event.preventDefault();
     setSearchQuery(event.target.value);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.get(`http://localhost:5000?q=${searchQuery}`);
+      const response = await axios.get(`http://localhost:5000/search?q=${searchQuery}`);
+      console.log(response.data.products)
       setProducts(response.data.products);
+      console.log(products)
     } catch (error) {
       console.log('Error:', error);
     }
@@ -21,10 +24,10 @@ const SearchProducts = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form>
        
         <input type="text" value={searchQuery} onChange={handleInputChange} class="form-control me-1" style={{ marginTop: '20px',borderRadius:'30px',alignContent:'centre',width:'400px',height:'60px',marginLeft:'492px' }}  placeholder="Type here" aria-label="submit"/>
-    <button  style={{height:'50px',width:'40px',borderRadius:'70px',marginTop:'-55px'}} size="lg">
+    <button onClick={handleSubmit} style={{height:'50px',width:'40px',borderRadius:'70px',marginTop:'-55px'}} size="lg">
     <i className="fa fa-search me-6" style={{fontSize:"27px",alignContent:'center',color:'gray',marginLeft:'-10px'}} aria-hidden="true"></i></button>
 
       </form>
